0d2d163db6062c5dc6b1217203488fd90961bb7d
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 01:32:26 +0700

    [ADD]04 - Aritmatika

---
+++ src/Aritmatika.java
@@ -0,0 +1,21 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Penjumlahan
+        System.out.println(a+b);
+
+        // Pengurangan
+        System.out.println(a-b);
+
+        // Perkalian
+        System.out.println(a*b);
+
+        // Pembagian
+        System.out.println(a/b);
+
+        // Modulus
+        System.out.println(a % b);
+    }
+}
