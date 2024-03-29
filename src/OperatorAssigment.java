356e4aa5c0f7ba932fdbe2a34d496e39ed30efbd
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 02:02:27 +0700

    [ADD] Operator Assigment

---
+++ src/OperatorAssigment.java
@@ -0,0 +1,25 @@
+public class OperatorAssigment {
+    public static void main(String[] args) {
+        //Assignment Operator Penjumlahan
+        int a = 5;
+        a += 5;
+        System.out.println("Nilai a = " + a);
+        //Assignment Operator Pengurangan
+        int b = 5;
+        b -= 3;
+        System.out.println("Nila b = " + b);
+        //Assigment Operator Perkalian
+        int c = 5;
+        c *= 3;
+        System.out.println("Nilai c = " + c);
+        //Assignment Operator Pembagian
+        int d = 100;
+        d /= 10;
+        System.out.println("Nilai d = " + d);
+        //Assignment Operator Modulus
+        int e = 5;
+        e %= 3;
+        System.out.println("Nilai e = " + e);
+
+    }
+}
