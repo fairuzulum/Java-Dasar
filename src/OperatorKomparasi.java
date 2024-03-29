ffe4636086fbb9c325cb041165dc879ba74a6df1
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 02:04:11 +0700

    [ADD] Operator Komparasi

---
+++ src/OperatorKomparasi.java
@@ -0,0 +1,21 @@
+public class OperatorKomparasi {
+    public static void main(String[] args) {
+         /*
+        == (sama dengan),
+        != (tidak sama dengan),
+        < (lebih kecil dari),
+        > (lebih besar dari),
+        <= (lebih kecil sama dengan),
+        >= (lebih besar sama dengan)
+
+
+       */
+        int a,b;
+        boolean hasilKomparasi;
+        a = 10;
+        b = 15;
+        hasilKomparasi = (a == b);
+        System.out.println(hasilKomparasi);
+
+    }
+}
