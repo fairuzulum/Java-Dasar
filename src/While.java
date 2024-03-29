a40c21d92ce7fad4115257a1bcc46ac61c161f9e
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 03:16:37 +0700

    [ADD]14 - While Loop

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        while (a <= b) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
