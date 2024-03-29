c88a546d6907d969680a9e4d00425717b985d0ab
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 01:21:20 +0700

    [ADD]02 - Variable

---
+++ src/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 10; // Assigment
+        int b; // Deklarasi
+        b = 5;
+        System.out.println(a+b);
+    }
+}
