1a03d2e91cffebf2f0b0fd301a43b28a7b2b2276
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-30 01:14:36 +0700

    [ADD] 01 - Printing dan Alur Eksekusi Java

---
+++ src/Printing.java
@@ -0,0 +1,7 @@
+public class Printing {
+    public static void main(String[] args) {
+        System.out.println("Baris 1");
+        System.out.print("Baris 2\n");
+        System.out.printf("Baris %d\n", 3);
+    }
+}
